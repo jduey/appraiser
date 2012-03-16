3f0bb2439ffbd300c17ae16062f9a4ef475e7578
Jim Duey <jim@intensivesystems.net>
2012-03-15 20:08:07 -0500

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject net.intensivesystems/appraiser "0.1.0-SNAPSHOT"
+  :description "appraiser: DSL's for web development"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/algo.monads "0.1.3-SNAPSHOT"]
+                 [org.clojure/java.jdbc "0.1.0"]
+                 [org.mindrot/jbcrypt "0.3m"]
+                 [mysql/mysql-connector-java "5.1.18"]
+                 [korma "0.3.0-beta6"]
+                 [compojure "0.6.5"]
+                 [ring "0.3.11"]])
